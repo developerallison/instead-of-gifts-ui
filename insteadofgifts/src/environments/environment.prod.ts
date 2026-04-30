858a671bffe925495aaedfb592f7e07f90a650c7
karthik <karthik.eg7591@gmail.com>
2026-04-29 22:00:05 -0400

    pkstripe

--- insteadofgifts/src/environments/environment.prod.ts
+++ insteadofgifts/src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment: AppEnvironment = {
     anonKey: 'sb_publishable_BCvxQBww6Pzkx4BMEjEvCA_GJREOu8B',
   },
   stripe: {
-    publishableKey: 'pk_test_51TLpjTK7vuC5QvGEEg3gQzkA910OMf8znTfz7ANIqHZQihqdzG2LNZ3dTmZaTHrjkqQK6JN10ckuYtCRqADIazwV00IeI951pE', //allison@insteadofgifts.com
+    publishableKey: 'pk_live_51TLpjEGosspEvv9AWRXV45I4nM94do8mmUxGSA2KW7DySvovvnyTAXy9iDj77vMukm1GQu5WYk2osuffYvFVJEOK00S2yjM07E', //allison@insteadofgifts.com
   },
   paypal: {
     clientId: 'AeeAp9_XzggG8bj4TA0GZZhOHBcyJLu1lLXx73mjDDN4-FnaE9pDoYH62Q3RgNxpAow1gpKHBi-7JnBM',
